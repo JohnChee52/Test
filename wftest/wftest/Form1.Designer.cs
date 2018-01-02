a8361183704983f6c9983fff17e0c89dfef9f525
John Chee <JohnChee52@gmail.com>
2018-01-02 17:38:15 +0800

    changes

--- wftest/wftest/Form1.Designer.cs
+++ wftest/wftest/Form1.Designer.cs
@@ -23,23 +23,29 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox_Test = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // comboBox_Test
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(53, 57);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 28);
-            this.comboBox1.TabIndex = 0;
+            this.comboBox_Test.FormattingEnabled = true;
+            this.comboBox_Test.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox_Test.Location = new System.Drawing.Point(53, 57);
+            this.comboBox_Test.Name = "comboBox_Test";
+            this.comboBox_Test.Size = new System.Drawing.Size(121, 28);
+            this.comboBox_Test.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(278, 244);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.comboBox_Test);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -49,7 +55,7 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox_Test;
     }
 }
 
